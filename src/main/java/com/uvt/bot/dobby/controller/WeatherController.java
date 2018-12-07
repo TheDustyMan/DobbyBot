2a15bc0defc4f635c9375575013eee7b1e1def0b
i353695 <andrei.popescu@sap.com>
2018-12-07 16:39:41 +0200

    Changed Weather Controller Added Recast Message

--- src/main/java/com/uvt/bot/dobby/controller/WeatherController.java
+++ src/main/java/com/uvt/bot/dobby/controller/WeatherController.java
@@ -3,18 +3,23 @@ package com.uvt.bot.dobby.controller;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.uvt.bot.dobby.model.DTO.JsonBodies.WeatherBody;
 import com.uvt.bot.dobby.model.DTO.JsonResponse;
+import com.uvt.bot.dobby.model.DTO.RecastMessage;
 import com.uvt.bot.dobby.model.DTO.TextResponse;
 import com.uvt.bot.dobby.services.WeatherService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
 
 @RestController
 public class WeatherController {
 
     private static final Logger logger = LoggerFactory.getLogger(WeatherController.class);
     private WeatherService weatherService;
+    private  RestTemplate restTemplate;
 
     @Autowired
     WeatherController(WeatherService weatherService){
@@ -22,14 +27,18 @@ public class WeatherController {
     }
 
     @PostMapping ("/getWeather")
-    public JsonResponse getWeather(@RequestBody JsonNode jsonNode){
+    public RecastMessage getWeather(@RequestBody JsonNode jsonNode){
 
-        String location = jsonNode.findValue("location").asText() + "," + jsonNode.findValue("country").asText();
+        String location = jsonNode.findValue("raw").asText() + "," + jsonNode.findValue("country").asText();
         WeatherBody weatherBody = weatherService.getWeather(location);
 
         String content = weatherBody.getTemperature() + "\n"+ weatherBody.getWeatherType();
 
-        return new TextResponse("text", content);
+        RecastMessage recastMessage = new RecastMessage(new ArrayList<JsonResponse>());
+
+        recastMessage.getMessages().add(new TextResponse("text", content));
+
+        return recastMessage;
     }
 
 }
